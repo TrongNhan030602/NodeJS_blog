496bc86cab5c846a99ea48515ea0ef153ecca41f
TrongNhan030602 <ntnhan030602@gmail.com>
2024-01-09 14:32:13 +0700

    Form GET/POSS

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public'))); //static file--> public
-// HTTP loger
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+// HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
@@ -16,12 +20,23 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// routing
 app.get('/', (req, res) => {
     res.render('home')
 });
+
 app.get('/news', (req, res) => {
     res.render('news');
 });
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+app.post('/search', (req, res) => {
+    console.log(req.body)
+    res.render('search');
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
