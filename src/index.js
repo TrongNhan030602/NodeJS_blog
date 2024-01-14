58bd88e1bb2799c2fe27205fb6032c457a03f2b2
TrongNhan030602 <ntnhan030602@gmail.com>
2024-01-14 20:49:21 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const route = require('./routes'); //Nap phia ngoai
 
 app.use(express.static(path.join(__dirname, 'public'))); //static file--> public
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -19,10 +19,10 @@ app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-  'hbs',
-  handlebars.engine({
-    extname: '.hbs',
-  }),
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -31,5 +31,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
